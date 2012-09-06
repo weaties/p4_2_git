a5cb9ce47ad2d0bb836318a385458e091c062dcc
Andy-Wu <whtandy@gmail.com>
2012-09-06 23:00:27 +0800

    Update git_cmd.rb
    with updated directory structure

--- git_cmd.rb
+++ git_cmd.rb
@@ -1,16 +1,28 @@
+@git_local_directory = "../workspace"
 
 #The directory of git project
-def git_init 
-  Dir.chdir("/opt/haitao/perforce/1953/www/expweb/trunk")
+def git_init
+  Dir.chdir(@git_local_directory)
+end
+
+def get_size dirs
+  depot_size = []
+  dirs.each do |dir|
+    size = `du -sh #{dir}`
+    depot_size << size[0,size.index("\t")]
+  end
+  depot_size
 end
 
 # commit the change.
 def git_commit message
   git_init
-  message.gsub!(/["]/,"'")  # deal the comments contain "
+  message.gsub!(/["]/,"'")  # deal the comments contain double question marks"
   `git add .`
   `git commit -m "#{message}"`
-  `du -sh`
+  git_push
+  dirs = ['../trunk.git/','../workspace/.git/','../workspace/']
+  get_size dirs
 end
 
 def git_status_time
@@ -19,7 +31,8 @@ def git_status_time
    `git status`
    Time.now - before
 end
-#TODO How to deal with the input of password issue, when push the change to remote server
+
 def git_push
   cc = `git push origin master`
 end
+
